b138711900543dd70fc0e8389f91bd29cdfc759c
Pratham Raj <130917920+imprathamraj@users.noreply.github.com>
2025-01-20 16:28:56 +0530

    Add files via upload

---
+++ Level1/ConstPerson.java
@@ -0,0 +1,26 @@
+/*Create a Person class with a copy constructor that clones another person's attributes.
+*/
+class Person {
+    String name;
+    int age;
+    
+    //Parameterized constructor
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    //Copy constructor
+    Person(Person p) {
+        this.name = p.name;
+        this.age = p.age;
+    }
+}
+public class ConstPerson {
+    public static void main(String[] args) {
+        Person p1 = new Person("John", 30);
+        Person p2 = new Person(p1);
+        System.out.println("Parameterized Constructor -  Name: " + p1.name + ", Age: " + p1.age);
+        System.out.println("Copy constructor - Name: " + p2.name + ", Age: " + p2.age);
+    }
+}
